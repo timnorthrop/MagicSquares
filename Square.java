203b8ce0baadfe9d51f18370cfdf23e81beace8d
Tim Northrop <tnorthg27@gmail.com>
2020-04-30 09:33:32 -0400

    Actual meat of some of the methods missing, but framework done

---
+++ Square.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Square here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Square
+{
+    private int dimension;
+    private int[][] data;
+    
+    public void magicSquare(int order){
+        dimension = order;
+        int[] d1 = new int[order*order];
+        for(int i = 1; i <= order*order; i++){
+            d1[i-1] = i;
+        }
+        for(int i = 0; i < order*order; i++){
+            int random = (int)Math.round(Math.random()*order*order);
+            int temp = d1[i];
+            d1[i] = d1[random];
+            d1[random] = temp;
+        }
+        for(int i = 0; i < order*order; i++){
+            
+        }
+    }
+    
+    public String toString(){
+        String toString = "";
+        for(int i = 0; i < dimension; i++){
+            
+        }
+        return toString;
+    }
+}
